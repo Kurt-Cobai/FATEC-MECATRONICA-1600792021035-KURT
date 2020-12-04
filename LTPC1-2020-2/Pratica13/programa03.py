2666de69f5a251d014b63b41e889d8721c848c3f
Kurt-Cobai <70581715+Kurt-Cobai@users.noreply.github.com>
2020-12-03 21:49:36 -0300

    Create programa03.py

---
+++ LTPC1-2020-2/Pratica13/programa03.py
@@ -0,0 +1,9 @@
+num1  =  float ( input ( "Informe um valor:" ))
+num2  =  float ( input ( "Informe outro valor:" ))
+
+se  num1  >  num2 :
+  imprimir ( "Maior:" , num1 )
+elif  num2  >  num1 :
+  imprimir ( "Maior:" , num2 )
+mais :
+  imprimir ( "São Iguais" )
