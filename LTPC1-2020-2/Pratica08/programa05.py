162d82b44ee50e9154f3a10f586f41238384600a
Kurt-Cobai <70581715+Kurt-Cobai@users.noreply.github.com>
2020-12-03 21:31:44 -0300

    Create programa05.py

---
+++ LTPC1-2020-2/Pratica08/programa05.py
@@ -0,0 +1,6 @@
+nome  =  input ( "Informe seu nome:" )
+
+posicao  =  0
+enquanto  posicao  <  len ( nome ):
+  print ( "Letra na posicao" , posicao , ":" , nome [ posicao ])
+  posicao  =  posicao  +  1
