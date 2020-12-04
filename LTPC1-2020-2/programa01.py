f8ca389ccf9019bca8ef801fa725c584e9ad46e2
Kurt-Cobai <70581715+Kurt-Cobai@users.noreply.github.com>
2020-12-03 21:27:55 -0300

    Create programa01.py

---
+++ LTPC1-2020-2/programa01.py
@@ -0,0 +1,11 @@
+  
+nome  =  input ( "Informe seu nome:" )
+time_de_futebol  =  input ( "Informe seu horário:" )
+quantidade  =  int ( input ( "Informe quantos mundiais o seu time venceu:" ))
+
+imprimir ( "Ola" , nome )
+
+if  time_de_futebol  ==  'Palmeiras' :
+  print ( "Seu time não tem mundial" )
+mais :
+  print ( "Seu tempo:" , time_de_futebol , "Venceu:" , quantidade )
