b993dfaf202729de63fedf293932192cd837c841
Kurt-Cobai <70581715+Kurt-Cobai@users.noreply.github.com>
2020-12-03 21:47:09 -0300

    Create programa04.py

---
+++ LTPC1-2020-2/Pratica12/programa04.py
@@ -0,0 +1,7 @@
+nome  =  input ( "Informe seu nome:" )
+valor  =  float ( input ( "Informe um valor:" ))
+
+# Método 1
+saida  =  "{}; {}" . formato ( nome . superior (), valor )
+
+print ( "Saida formatada:" , saida )
