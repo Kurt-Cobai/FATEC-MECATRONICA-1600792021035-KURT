94963b204b47ecacc483418d098334a4546f8f92
Kurt-Cobai <70581715+Kurt-Cobai@users.noreply.github.com>
2020-12-03 21:22:57 -0300

    Create programa03.py

---
+++ LTPC1-2020-2/Pratica07/programa03.py
@@ -0,0 +1,11 @@
+continuar  =  verdadeiro
+enquanto  continuar  ==  Verdadeiro :
+  nome  =  input ( "Informe seu nome:" )
+  if  nome  ==  'Kuririn' :
+    continuar  =  False
+  mais :
+    ki  =  int ( input ( "Informe seu poder de luta:" ))
+    se  ki  >  8000 :
+      imprimir ( "Mais forte que o Goku!" )
+    mais :
+      imprimir ( "Procura o Vegeta e vai treinar!" )
