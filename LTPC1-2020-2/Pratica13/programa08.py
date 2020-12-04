e1e21be84941479007069a3032be30fc46a6ceac
Kurt-Cobai <70581715+Kurt-Cobai@users.noreply.github.com>
2020-12-03 21:52:10 -0300

    Create programa08.py

---
+++ LTPC1-2020-2/Pratica13/programa08.py
@@ -0,0 +1,9 @@
+  
+numero  =  42
+imprimir ( tipo ( numero ))
+imprimir ( numero )
+imprimir ( numero  +  2 )
+texto  =  str ( numero )
+imprimir ( tipo ( texto ))
+imprimir ( texto )
+imprimir ( texto  +  '2' )
